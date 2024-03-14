e57a3f8a3ee30920d760c5d75c0ecf8928602494
Vishesh1903 <visheshupreti32@gmail.com>
2024-03-15 01:23:56 +0530

    UI part completed

--- public/upsurge-navbar/src/components/Navbar/Navbar.jsx
+++ public/upsurge-navbar/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import search_icon_light from "../../assets/search-w.png";
 import search_icon_dark from "../../assets/search-b.png";
 import toggle_light from "../../assets/night.png";
 import toggle_dark from "../../assets/day.png";
-import { IoIosArrowDropup } from "react-icons/io";
+import { IoIosArrowDropupCircle } from "react-icons/io";
 
 const Navbar = ({ theme, setTheme }) => {
   const toggle_mode = () => {
@@ -19,10 +19,10 @@ const Navbar = ({ theme, setTheme }) => {
       </div>
 
       <div className="navlinks">
-        <div>
-          <div className="arrow-div">Learn</div>
-          <div>
-            <IoIosArrowDropup className="arrow" />
+        <div className="learn-container">
+          <div>Learn</div>
+          <div className="arrow-div">
+            <IoIosArrowDropupCircle  className="arrow" />
           </div>
         </div>
 
